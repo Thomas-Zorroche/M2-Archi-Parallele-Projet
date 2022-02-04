4477010023e2d5e2a58a44df60165031bbacf084
Félix <felix.olart56@gmail.com>
2022-02-04 09:10:28 +0100

    [Filter] Other sobel kernels

--- src/sobel.hpp
+++ src/sobel.hpp
@@ -3,8 +3,10 @@
 #include "utils.hpp"
 
 // Filtres
+// static float dataSobelX[] = {1, 0, -1, 2, 0, -2, 1, 0, -1};
 static float dataSobelX[] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
 static const Kernel sobelX = {dataSobelX, 3, 3};
+// static float dataSobelY[] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
 static float dataSobelY[] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
 static const Kernel sobelY = {dataSobelY, 3, 3};
 
